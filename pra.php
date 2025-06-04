573581bcce48ba763010ae2749ea89b5af2cf13b
Smriti Poudel <smritipoudel0607@gmail.com>
2025-06-04 13:46:15 +0545

    "regex"

--- pra.php
+++ pra.php
@@ -45,7 +45,7 @@
         // echo $n;
 
         // $fruits=['apple','banana','mango','watermelon'];
-        // array_splice($fruits,0,2);// mango, watermelon(output)
+        // array_splice($fruits,1,2);// mango, watermelon(output)--0=index, 2=number of elements to remove
         // print_r($fruits);
 
     //  $fruits=['apple'=>'pp','banana'=>'po','mango'=>'lo','watermelon'=>'ko'];
@@ -53,10 +53,45 @@
     //     var_dump($ll);
     //     print_r($ll);
 
-    $lp=["kk","po","ii"];
-    $pp=array_shift($lp);//po,li
-    var_dump($lp) ;
-        ?>
+    // $lp=["kk","po","ii"];
+    // $pp=array_shift($lp);//po,li
+    // var_dump($lp) ;
+//  $a=[1,2,3];
+//  $b=array_shift($a);
+//  echo $b; // 1
+//  print_r($a); // 2,3 
+
+
+    //super global 
+    // $a=10;
+    // print_r($GLOBALS); //GET,POST,COOKIES, FILES..(give all the global variables from this file)
+    // print_r($_SERVER); // give all the server variables
+    // print_r($_SESSION); // give all the session variables
+    // print_r($_COOKIE); // give all the cookie variables
+    // print_r($_FILES); // give all the file variables
+    // print_r($_REQUEST); // give all the request variables
+    // print_r($_ENV); // give all the environment variables
+
+    // $a=10;
+    // function test(){
+    //     global $a;
+    //     echo $a;
+    // }
+    // test();
+
+//     //Regrex
+//     $string="hello php , how are you";
+//     $exp=preg_match_all("/o/i",$string,$array);
+//    if ($exp){
+//     echo " A match found";
+//    }else{
+//     echo " No match found";
+//    }
+// //    echo"<pre>";
+//    print_r($array);
+// //    echo "</pre>";
+
+    ?>
     </pre>
 
 </body>
