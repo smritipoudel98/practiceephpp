8e66216ce0537afa7fe26ffe06841adae84e178a
Smriti Poudel <smritipoudel0607@gmail.com>
2025-06-05 13:50:45 +0545

    fopen(),fclose(),fgets()

--- file.php
+++ file.php
@@ -1,6 +1,12 @@
 <?php
+echo"<pre>";
 
 $myFile=fopen("sample.txt","r");
-echo fread($myFile,filesize("sample.txt"));
+// echo fread($myFile,filesize("sample.txt"));
+echo fgets($myFile);//(give one line of sentence.)
+echo fgets($myFile);//(give second line of sentence.)
+echo fgets($myFile);//(give third line of sentence.)
+
 fclose($myFile);
+echo "</pre>";
 ?>
