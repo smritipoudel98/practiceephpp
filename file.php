073e8f08af9a8c22cf740ba5e8cc2726049850e2
Smriti Poudel <smritipoudel0607@gmail.com>
2025-06-05 14:25:47 +0545

    fgets(),fgetc()

--- file.php
+++ file.php
@@ -8,5 +8,18 @@ echo fgets($myFile);//(give second line of sentence.)
 echo fgets($myFile);//(give third line of sentence.)
 
 fclose($myFile);
+echo "<br>";
+
+$file = fopen("sample.txt", "r");
+while (!feof($file)) {
+    $char = fgetc($file);
+    echo $char;
+    
+    if ($char == 'o') {
+        break;
+    }
+}
+fclose($file);
+
 echo "</pre>";
 ?>
